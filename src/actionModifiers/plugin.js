229bec9b1e0bdbc99c30f47d5c50ba49f1f85181
David Tsay <david.e.tsay@gmail.com>
2023-04-05 14:40:27 -0700

    scrap export modifier

--- src/actionModifiers/plugin.js
+++ src/actionModifiers/plugin.js
@@ -1,16 +1,13 @@
 import preventImportIntoDatasetModifier from './preventImportIntoDatasetModifier';
-import exportWithDatasetsModifier from './ImportExportWithDatasets/exportWithDatasetsModifier';
 import importWithDatasetsModifier from './ImportExportWithDatasets/importWithDatasetsModifier';
 
-function plugin() {
+function ImportExportWithDatasetsPlugin() {
     return function install(openmct) {
-
         openmct.on('start', () => {
             preventImportIntoDatasetModifier(openmct);
-            exportWithDatasetsModifier(openmct);
             importWithDatasetsModifier(openmct);
         });
     }
 }
 
-export default plugin;
+export default ImportExportWithDatasetsPlugin;
