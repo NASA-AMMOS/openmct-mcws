034ad3a3b0b5df8081a683b1771fedcb503c35dc
Jamie V <jamie.j.vigliotta@nasa.gov>
2023-05-19 16:45:01 -0700

    dont set the localStorage item thousands of times on the first run

--- src/persistence/existingNamespaceUpdateInterceptor.js
+++ src/persistence/existingNamespaceUpdateInterceptor.js
@@ -1,17 +1,20 @@
 export default function existingNamespaceUpdateInterceptor(openmct, usersNamespace, shouldCheck) {
+    let turnedOff = false;
+
     openmct.objects.addGetInterceptor({
         appliesTo: (identifier, domainObject) => {
-            console.log('check?', shouldCheck);
             return shouldCheck;
         },
         invoke: (identifier, object) => {
             if (object.location === usersNamespace.key) {
                 object.location = usersNamespace.id;
                 openmct.objects.mutate(object, 'location', usersNamespace.id);
-                console.log(identifier, object);
 
                 // don't check ever again, just don't
-                localStorage.setItem('r5.0_old_namespaces_checked', 'true');
+                if (!turnedOff) {
+                    localStorage.setItem('r5.0_old_namespaces_checked', 'true');
+                    turnedOff = true;
+                }
             }
 
             return object;
