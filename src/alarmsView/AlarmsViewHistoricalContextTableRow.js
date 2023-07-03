675e68b665e5fc577e94ca59f46c8ff592d3604e
David Tsay <david.e.tsay@gmail.com>
2023-07-03 11:03:12 -0700

    scope variable to function

--- src/alarmsView/AlarmsViewHistoricalContextTableRow.js
+++ src/alarmsView/AlarmsViewHistoricalContextTableRow.js
@@ -2,7 +2,6 @@ import TelemetryTableRow from 'openmct.tables.TelemetryTableRow';
 import DatasetCache from 'services/dataset/DatasetCache';
 import Types from 'types/types';
 
-const channelType = Types.typeForKey('vista.channel');
 export default class AlarmsViewHistoricalContextTableRow extends TelemetryTableRow {
     getContextualDomainObject(openmct, objectKeyString) {
         const identifier = openmct.objects.parseKeyString(objectKeyString);
@@ -11,6 +10,7 @@ export default class AlarmsViewHistoricalContextTableRow extends TelemetryTableR
         const datasetIdentifier = data.datasetIdentifier;
         
         return DatasetCache().get(datasetIdentifier).then(dataset => {
+            const channelType = Types.typeForKey('vista.channel');
             const objectKeyString = 'vista:' + channelType.makeId(dataset.identifier, this.datum.channel_id);
 
             return openmct.objects.get(objectKeyString);
