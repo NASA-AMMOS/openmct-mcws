116ecfe88f6c0b04d5a8193b4cdb907bba580dd5
David Tsay <david.e.tsay@gmail.com>
2023-08-18 15:50:03 -0700

    warn if duplicating datasets

---
+++ src/actionModifiers/MultipleDatasets/warnMultipleDatasetsOnDuplicateModifier.js
@@ -0,0 +1,48 @@
+import DatasetCache from 'services/dataset/DatasetCache';
+
+export default function warnMultipleDatasetsModifier(openmct) {
+  const duplicateAction = openmct.actions._allActions['duplicate'];
+
+  if (duplicateAction) {
+    const originalOnSaveFunction = duplicateAction.onSave.bind(duplicateAction);
+
+    duplicateAction.onSave = async (object, parent, changes) => {
+      const datasetCache = DatasetCache();
+      const datasets = await datasetCache.getDomainObjects();
+
+      const isDuplicatingDataset = datasets.some(dataset => openmct.objects.areIdsEqual(
+        dataset.identifier, object.identifier)
+      );
+
+      const confirmDuplicateDataset = () => {
+        const dialog = openmct.overlays.dialog({
+          iconClass: 'alert',
+          message: `Datasets should provide unique data.
+            A valid usecase for adding an additional dataset would be splitting all data into separate pre-indexed data.
+          `,
+          buttons: [
+            {
+              label: 'OK',
+              callback: () => {
+                dialog.dismiss();
+                originalOnSaveFunction(object, parent, changes);
+              }
+            },
+            {
+              label: 'Cancel',
+              callback: () => {
+                dialog.dismiss();
+              }
+            }
+          ]
+        });
+      };
+
+      if (isDuplicatingDataset) {
+        confirmDuplicateDataset();
+      } else {
+        originalOnSaveFunction(object, parent, changes);
+      }
+    };
+  }
+};
--- src/actionModifiers/plugin.js
+++ src/actionModifiers/plugin.js
@@ -1,13 +1,18 @@
 import preventImportIntoDatasetModifier from './preventImportIntoDatasetModifier';
 import importWithDatasetsModifier from './ImportExportWithDatasets/importWithDatasetsModifier';
+import warnMultipleDatasetsOnDuplicateModifier from './MultipleDatasets/warnMultipleDatasetsOnDuplicateModifier';
 
-function ImportExportWithDatasetsPlugin() {
+/**
+ * DEPENDENCY: These modifiers have a dependency on Open MCT action internals.
+ */
+function ActionModifiersPlugin() {
     return function install(openmct) {
         openmct.on('start', () => {
             preventImportIntoDatasetModifier(openmct);
             importWithDatasetsModifier(openmct);
+            warnMultipleDatasetsOnDuplicateModifier(openmct);
         });
     }
 }
 
-export default ImportExportWithDatasetsPlugin;
+export default ActionModifiersPlugin;
