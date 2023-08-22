18cf12c90e768c615b4b854ed1dc80c3af48e77c
Jamie V <jamie.j.vigliotta@nasa.gov>
2023-08-22 15:33:08 -0700

    removing debug

--- src/persistence/missingUserFolderInterceptor.js
+++ src/persistence/missingUserFolderInterceptor.js
@@ -6,8 +6,6 @@ export default function missingUserFolderInterceptor(openmct, usersNamespace) {
 
     openmct.objects.addGetInterceptor({
         appliesTo: (identifier, domainObject) => {
-            console.log('usersNamespace', usersNamespace);
-            console.log(identifier, !domainObject && userKeyCheck.test(identifier.namespace));
             return !domainObject && userKeyCheck.test(identifier.namespace);
         },
         invoke: (identifier, object) => {
