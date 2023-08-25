fea267aa89f5e55c1371c92c0195fe4a08434bb3
Jamie V <jamie.j.vigliotta@nasa.gov>
2023-08-25 09:51:07 -0700

    sigh... typo

--- src/persistence/missingUserFolderInterceptor.js
+++ src/persistence/missingUserFolderInterceptor.js
@@ -9,7 +9,7 @@ export default async function missingUserFolderInterceptor(openmct, usersNamespa
 
     openmct.objects.addGetInterceptor({
         appliesTo: (identifier, domainObject) => {
-            return containedIds.find(id => openmct.objects.arIdsEqual(id, identifier));
+            return containedIds.find(id => openmct.objects.areIdsEqual(id, identifier));
         },
         invoke: (identifier, object) => {
             const namspaceParts = identifier.namespace.split('-');
