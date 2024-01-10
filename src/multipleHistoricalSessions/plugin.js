4207986077012ecfe8d2d15fb5532ccc2ff55d13
David Tsay <david.e.tsay@gmail.com>
2024-01-09 17:45:51 -0800

    remove injector and fix historical session indicator

--- src/multipleHistoricalSessions/plugin.js
+++ src/multipleHistoricalSessions/plugin.js
@@ -5,53 +5,48 @@ import HistoricalSessionMetadata from './HistoricalSessionMetadata';
 
 export default function HistoricalSessionsPlugin() {
     return function install(openmct) {
-        openmct.on('start', () => {
-            const instantiate = openmct.$injector.get('instantiate');
-            const model = {
-                identifier: {
-                    key: 'session-historical',
-                    namespace: ''
-                },
-                name: 'Historical Session',
-                type: 'vista.channel'
-            };
-            const oldStyleDomainObject = instantiate(model);
-            const newStyleDomainObject = oldStyleDomainObject.useCapability('adapter');
-            const table = new SessionTable(
-                newStyleDomainObject,
+        const domainObject = {
+            identifier: {
+                key: 'session-historical',
+                namespace: ''
+            },
+            name: 'Historical Session',
+            type: 'vista.channel'
+        };
+
+        const table = new SessionTable(
+            domainObject,
+            openmct,
+            HistoricalSessionMetadata
+        );
+        const objectPath = [ domainObject ];
+
+        const componentDefinition = {
+            provide: {
                 openmct,
-                HistoricalSessionMetadata
-            );
-            const objectPath = [ model ];
+                table,
+                objectPath,
+                currentView: {}
+            },
+            components: {
+                HistoricalSessionIndicator
+            },
+            template: '<HistoricalSessionIndicator></HistoricalSessionIndicator>'
+        };
 
-            const componentDefinition = {
-                provide: {
-                    openmct,
-                    table,
-                    objectPath,
-                    currentView: {}
-                },
-                components: {
-                    HistoricalSessionIndicator
-                },
-                template: '<HistoricalSessionIndicator></HistoricalSessionIndicator>'
-            };
+        const {
+            componentInstance,
+            destroy,
+            el
+        } = mount(componentDefinition);
 
-            const {
-                componentInstance,
-                destroy,
-                el
-            } = mount(componentDefinition);
+        const indicator = {
+            key: 'historical-session-indicator',
+            element: el,
+            priority: -1,
+            destroy
+        };
 
-            const indicator = {
-                key: 'historical-session-indicator',
-                element: el,
-                priority: -1,
-                destroy
-            };
-
-            openmct.indicators.add(indicator);
-        });
-
+        openmct.indicators.add(indicator);
     };
 }
