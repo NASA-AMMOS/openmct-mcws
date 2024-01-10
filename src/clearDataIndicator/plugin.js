40bbb42aac5017b4db0316ed8198e44ac4f7cf9b
David Tsay <david.e.tsay@gmail.com>
2024-01-09 16:11:11 -0800

    fix missing indicator element

--- src/clearDataIndicator/plugin.js
+++ src/clearDataIndicator/plugin.js
@@ -3,24 +3,27 @@ import ClearDataIndicator from './indicator/clearDataIndicator.vue';
 
 export default function plugin(globalStalenessMs) {
     return function install(openmct) {
+        const indicator = {
+            element: document.createElement('div')
+        };
+        const componentDefinition = {
+            provide: {
+                openmct,
+                globalStalenessMs
+            },
+            components: {
+                ClearDataIndicator
+            },
+            template: '<ClearDataIndicator />'
+        };
+        const componentOptions = {
+            element: indicator.element
+        };
+
         openmct.indicators.add(indicator);
-        openmct.on('start', () => {
-            const componentDefinition = {
-                provide: {
-                    openmct,
-                    globalStalenessMs
-                },
-                components: {
-                    ClearDataIndicator
-                },
-                template: '<ClearDataIndicator />'
-            };
-            
-            const {
-              componentInstance,
-              destroy,
-              el
-            } = mount(componentDefinition);
-        });
+        openmct.on(
+            'start',
+            () => mount(componentDefinition, componentOptions)
+        );
     };
 }
