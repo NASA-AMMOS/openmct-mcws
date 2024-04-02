c1ec596d0b6af1ee142aed0cd7b5446c9cda398a
Jamie V <jamie.j.vigliotta@nasa.gov>
2024-04-02 12:57:11 -0700

    convert venue template and controller to vue file and venue plugin to es6

---
+++ src/venues/VenueComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    class="c-venue-button c-button"
+    :class="{
+      loading: isLoading,
+      'is-selected': isSelected(venue),
+      'is-active': isActive
+    }"
+    @click="selectVenue(venue)"
+    :disabled="!isActive"
+  >
+    <span class="c-venue-button__label">{{ name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['parameters'],
+  data() {
+    return {
+      venue: this.parameters.venue,
+      isSelected: this.parameters.isSelected,
+      selectVenue: this.parameters.selectVenue,
+      name: '',
+      isActive: false,
+      isLoading: false,
+    };
+  },
+  created() {
+    this.name = this.venue.model.name;
+    
+    if (this.venue.allowsRealtime()) {
+      this.isLoading = true;
+      this.venue.getActiveSessions()
+        .then((activeSessions) => {
+          if (activeSessions.length) {
+            this.isActive = true;
+          }
+        })
+        .catch((error) => {
+          console.error('Error loading active sessions for Venue', this.venue);
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    }
+  },
+};
+</script>
+
+<style scoped>
+/* Add your CSS styles if any */
+</style>
