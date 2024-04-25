773a1b2f6b3b84ee3b3980ca096e72e6cb8e4b32
Jamie V <jamie.j.vigliotta@nasa.gov>
2024-04-25 15:48:54 -0700

    removed development settings

--- config.js
+++ config.js
@@ -486,8 +486,8 @@
          * they do!
          */
         assetPath: 'node_modules/openmct/dist',
-        proxyUrl: 'http://localhost:8080/',
-        useDeveloperStorage: true
+        // proxyUrl: 'http://localhost:8080/',
+        // useDeveloperStorage: true
     };
 
     window.openmctMCWSConfig = openmctMCWSConfig;
