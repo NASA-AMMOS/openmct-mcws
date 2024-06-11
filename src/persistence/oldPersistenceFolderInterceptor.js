99e91df4670d33d9afe57c48bc680f1497e626e8
Jamie V <jamie.j.vigliotta@nasa.gov>
2024-06-11 16:18:49 -0700

    no need to have a usernamespace, must work without one

--- src/persistence/oldPersistenceFolderInterceptor.js
+++ src/persistence/oldPersistenceFolderInterceptor.js
@@ -3,46 +3,47 @@ import {
   interpolateUsername
 } from './utils';
 
-export default async function oldPersistenceFolderInterceptor(openmct, namespaces, ROOT_IDENTIFIERS) {
-    let usersNamespace = namespaces.find((namespace) => namespace.containsNamespaces);
-    usersNamespace = structuredClone(usersNamespace);
+function isUserNamespace(namespace, userKeyRegex, identifier) {
+  return namespace && userKeyRegex.test(identifier.namespace);
+}
 
-    const userTemplate = usersNamespace.childTemplate.key.split('$')[0];
-    const userKeyCheck = new RegExp(`^${userTemplate}([^:]+)$`);
+function findNamespaceDefinition(namespaces, identifier) {
+  return namespaces.find(namespace => namespace.key === identifier.namespace);
+}
+
+export default async function oldPersistenceFolderInterceptor(openmct, namespaces, ROOT_IDENTIFIERS) {
+    const usersNamespace = namespaces.find(namespace => namespace.containsNamespaces);
+    const userKeyRegex = usersNamespace ? new RegExp(`^${usersNamespace.childTemplate.key.split('$')[0]}([^:]+)$`) : null;
 
     openmct.objects.addGetInterceptor({
         appliesTo: (identifier, domainObject) => {
             const isMissing = !domainObject;
             const isNotUserRoot = identifier.key !== 'container';
-            const isUserFolderIdentifier = userKeyCheck.test(identifier.namespace);
+            const isUserFolderIdentifier = isUserNamespace(usersNamespace, userKeyRegex, identifier);
             const nonUserRoots = ROOT_IDENTIFIERS.filter(rootId => rootId.key !== 'container');
-            const isRootFolder = nonUserRoots.find(rootId => openmct.objects.areIdsEqual(rootId, identifier)) !== undefined;
+            const isRootFolder = nonUserRoots.some(rootId => openmct.objects.areIdsEqual(rootId, identifier));
 
             // we will create domain objects for empty older persistence user or
             // root (ex: shared) folders, not for the user root or other folders
             return isMissing && isNotUserRoot && (isUserFolderIdentifier || isRootFolder);
         },
         invoke: (identifier, object) => {
-            const isUserFolderIdentifier = userKeyCheck.test(identifier.namespace) && !identifier.namespace.includes('shared');
-
-            let userId;
+            let userId = 'system';
             let namespaceDefinition;
-            
-            if (isUserFolderIdentifier) {
-                userId = identifier.namespace.match(userKeyCheck)[1];
+
+            if (isUserNamespace(usersNamespace, userKeyRegex, identifier) && !identifier.namespace.includes('shared')) {
+                userId = identifier.namespace.match(userKeyRegex)[1];
+
                 namespaceDefinition = interpolateUsername(usersNamespace.childTemplate, userId);
                 namespaceDefinition.location = usersNamespace.id;
             } else {
-                userId = 'system';
-                namespaceDefinition = namespaces.find(namespace => namespace.key === identifier.namespace);
+                namespaceDefinition = findNamespaceDefinition(namespaces, identifier) || { key: 'default' };
             }
 
             const model = createModelFromNamespaceDefinition(userId, namespaceDefinition);
-            
             openmct.objects.save(model);
-            
+
             return model;
         }
     });
 }
-
