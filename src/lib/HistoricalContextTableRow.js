9244cbae973c1f46fbbfc7ad3d6ddd499c128388
David Tsay <3614296+davetsay@users.noreply.github.com>
2024-07-29 13:29:48 -0700

    remove unnecessary constructor

--- src/lib/HistoricalContextTableRow.js
+++ src/lib/HistoricalContextTableRow.js
@@ -1,10 +1,6 @@
 import TelemetryTableRow from 'openmct.tables.TelemetryTableRow';
 
 export default class HistoricalContextTableRow extends TelemetryTableRow {
-  constructor(datum, columns, objectKeyString, limitEvaluator) {
-    super(datum, columns, objectKeyString, limitEvaluator);
-  }
-
   getContextMenuActions() {
     return ['viewHistoricalData'];
   }
