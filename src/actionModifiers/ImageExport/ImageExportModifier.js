12cd5327661c6beae68e5291f9fe59cae3b9a8b9
Jamie V <jamie.j.vigliotta@nasa.gov>
2025-05-21 12:16:58 -0700

    update core dependency, add the imageexport modifer to our action modifier plugin

--- src/actionModifiers/ImageExport/ImageExportModifier.js
+++ src/actionModifiers/ImageExport/ImageExportModifier.js
@@ -1,15 +1,37 @@
 import SessionService from 'services/session/SessionService';
+import { formatNumberSequence } from 'ommUtils/strings';
 
 function imageExportModifier(openmct) {
   const PNGImageExportAction = openmct.actions._allActions['export-as-png'];
   const JPGImageExportAction = openmct.actions._allActions['export-as-jpg'];
   const imageExportActions = [PNGImageExportAction, JPGImageExportAction].filter(Boolean);
+  const sessionService = SessionService();
 
   imageExportActions.forEach((action) => {
-    action.appliesTo = (objectPath, view = {}) => {
-      return isPlotView(view);
+    const invoke = action.invoke;
+
+    action.invoke = (objectPath, view) => {
+      let filename = objectPath[0].name;
+      const sessionFilter = sessionService.getHistoricalSessionFilter();
+
+      if (sessionFilter) {
+        filename = `${filename} - ${historicalFilterString(sessionFilter)}`;
+      }
+
+      filename = `${filename} - plot`;
+
+      invoke(objectPath, view, filename);
     };
   });
+}
+
+function historicalFilterString(sessionFilter) {
+  let filterString = formatNumberSequence(sessionFilter.numbers);
+
+  filterString = filterString.replaceAll('...', '-');
+  filterString = filterString.replaceAll(', ', '_');
+
+  return `${sessionFilter.host}_${filterString}`;
 }
 
 export default imageExportModifier;
