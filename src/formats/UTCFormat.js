14f29772e49088c8141573f9aa9cd023ed03df18
David Tsay <david.e.tsay@gmail.com>
2025-10-28 14:40:16 -0700

    convert formatters to es6 modules

--- src/formats/UTCFormat.js
+++ src/formats/UTCFormat.js
@@ -55,49 +55,48 @@ function getScaledFormat(m) {
  * @implements {Format}
  * @constructor
  */
-function UTCFormat() {
-  this.key = 'utc';
-}
+export default class UTCFormat {
+  constructor() {
+    this.key = 'utc';
+    this.FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSS';
+    this.ACCEPTABLE_FORMATS = [
+      UTCFormat.prototype.FORMAT,
+      'YYYY-MM-DDTHH:mm:ss',
+      'YYYY-MM-DDTHH:mm',
+      'YYYY-MM-DDTHH',
+      'YYYY-MM-DD',
+      'YYYY-DDDDTHH:mm:ss.SSS',
+      'YYYY-DDDTHH:mm:ss',
+      'YYYY-DDDTHH:mm',
+      'YYYY-DDDTHH',
+      'YYYY-DDD'
+    ];
+  }
 
-UTCFormat.prototype.FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSS';
-UTCFormat.prototype.ACCEPTABLE_FORMATS = [
-  UTCFormat.prototype.FORMAT,
-  'YYYY-MM-DDTHH:mm:ss',
-  'YYYY-MM-DDTHH:mm',
-  'YYYY-MM-DDTHH',
-  'YYYY-MM-DD',
-  'YYYY-DDDDTHH:mm:ss.SSS',
-  'YYYY-DDDTHH:mm:ss',
-  'YYYY-DDDTHH:mm',
-  'YYYY-DDDTHH',
-  'YYYY-DDD'
-];
-
-UTCFormat.prototype.format = function (value, scale) {
-  var m = moment.utc(value);
-  if (typeof scale !== 'undefined') {
-    var scaledFormat = getScaledFormat(m);
-    if (scaledFormat) {
-      return m.format(scaledFormat);
+  format(value, scale) {
+    var m = moment.utc(value);
+    if (typeof scale !== 'undefined') {
+      var scaledFormat = getScaledFormat(m);
+      if (scaledFormat) {
+        return m.format(scaledFormat);
+      }
     }
-  }
-  return m.format(this.FORMAT);
-};
-
-UTCFormat.prototype.parse = function (text) {
-  if (text === undefined || typeof text === 'number') {
-    return text;
+    return m.format(this.FORMAT);
   }
 
-  if (DOY_PATTERN.test(text)) {
-    return +inlineParseDOYString(text);
+  parse(text) {
+    if (text === undefined || typeof text === 'number') {
+      return text;
+    }
+
+    if (DOY_PATTERN.test(text)) {
+      return +inlineParseDOYString(text);
+    }
+
+    return moment.utc(text, this.ACCEPTABLE_FORMATS, true).valueOf();
   }
 
-  return moment.utc(text, this.ACCEPTABLE_FORMATS, true).valueOf();
-};
-
-UTCFormat.prototype.validate = function (text) {
-  return text !== undefined && moment.utc(text, this.ACCEPTABLE_FORMATS, true).isValid();
-};
-
-export default UTCFormat;
+  validate(text) {
+    return text !== undefined && moment.utc(text, this.ACCEPTABLE_FORMATS, true).isValid();
+  }
+}
