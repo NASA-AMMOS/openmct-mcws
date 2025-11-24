9b93d5ba9555830870234c68617f37952bbaa804
Jamie V <jamie.j.vigliotta@nasa.gov>
2025-11-24 11:12:43 -0800

    remove instances

--- instances/development/assets/installBuiltinPlugin.js
+++
@@ -1,9 +0,0 @@
-import { substituteVariables, runtimeSubstitutions } from "./mct-builder-core.js";
-
-export default async function installBuiltinPlugin({openmct, installFunction, installFunctionOptions, buildTimeSubstitutions}) {
-    const optionsWithSubstitutions = substituteVariables(installFunctionOptions, {
-        ...buildTimeSubstitutions,
-        ...runtimeSubstitutions
-    });
-    openmct.install(installFunction(optionsWithSubstitutions));
-}
