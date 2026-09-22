a9ad20fab0d6b07768050446e6ab2621b1d0b828
Jamie V. <jamie.j.vigliotta@nasa.gov>
2026-09-22 13:32:50 -0700

    Backmerge 6.0.1, version update, small fix from scan (#479)
    Co-authored-by: David Tsay <3614296+davetsay@users.noreply.github.com>

--- src/formats/SCLKFloat64FormatSpec.js
+++ src/formats/SCLKFloat64FormatSpec.js
@@ -1,31 +1,29 @@
-define(['./SCLKFloat64Format'], function (SCLKFloat64Format) {
-  'use strict';
+import SCLKFloat64Format from './SCLKFloat64Format.js';
 
-  describe('SCLKFormat', function () {
-    var format;
+describe('SCLKFormat', () => {
+  let format;
 
-    beforeEach(function () {
-      format = new SCLKFloat64Format();
-    });
+  beforeEach(() => {
+    format = new SCLKFloat64Format();
+  });
 
-    it('can parse values', function () {
-      expect(format.parse('5.0410090875E8')).toBe(504100908.75);
-      expect(format.parse('504100908.75')).toBe(504100908.75);
-      expect(format.parse('5.0410093875E8')).toBe(504100938.75);
-      expect(format.parse('504100938.75')).toBe(504100938.75);
-      expect(format.parse(504100938.75)).toBe(504100938.75);
-    });
+  it('can parse values', function () {
+    expect(format.parse('5.0410090875E8')).toBe(504100908.75);
+    expect(format.parse('504100908.75')).toBe(504100908.75);
+    expect(format.parse('5.0410093875E8')).toBe(504100938.75);
+    expect(format.parse('504100938.75')).toBe(504100938.75);
+    expect(format.parse(504100938.75)).toBe(504100938.75);
+  });
 
-    it('can format values', function () {
-      expect(format.format(504100908.75)).toBe('504100908.75');
-      expect(format.format(504100938.75)).toBe('504100938.75');
-    });
+  it('can format values', function () {
+    expect(format.format(504100908.75)).toBe('504100908.75');
+    expect(format.format(504100938.75)).toBe('504100938.75');
+  });
 
-    it('can validate values', function () {
-      expect(format.validate('5.0410090875E8')).toBe(true);
-      expect(format.validate('504100908.75')).toBe(true);
-      expect(format.validate('hello')).toBe(false);
-      expect(format.validate('0.0')).toBe(true);
-    });
+  it('can validate values', function () {
+    expect(format.validate('5.0410090875E8')).toBe(true);
+    expect(format.validate('504100908.75')).toBe(true);
+    expect(format.validate('hello')).toBe(false);
+    expect(format.validate('0.0')).toBe(true);
   });
 });
